8bf67817bc38faaf5edeea7e46b9cd009601a0bf
Smruti Ranjan Samal <smrutiranjansamal2004537@gmail.com>
2026-08-09 23:39:52 +0530

    Update 0014-longest-common-prefix.py

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -2,10 +2,6 @@ class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         pre = strs[0]
 
-class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
-        pre = strs[0]
-
         for i in strs[1:]:
             while not i.startswith(pre):
                 pre = pre[:-1]
